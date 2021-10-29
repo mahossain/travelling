c6e21d56d281b14d2aaa634be6652af1243eb2f4
Afzal Hossain <mhossain2@woolworths.com.au>
2021-10-29 12:58:19 +1100

    added test

---
+++ src/__tests__/util.test.js
@@ -0,0 +1,8 @@
+describe('util tests', () => {
+  test('adding 2 + 2 = 4', async () => {
+    expect(2+2).toEqual(4);
+  });
+  test('adding 5 * 5 = 25', async () => {
+    expect(5 * 5).toEqual(25);
+  });
+});
