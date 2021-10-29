0392214fd1c40513aa8ed01ab1d58a7785427218
Afzal Hossain <mhossain2@woolworths.com.au>
2021-10-29 12:56:12 +1100

    travelling app

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 8000
+
+const app = express()
+  .use(cors())
+  .use(bodyParser.json());
+
+app.get('/wx/travel/locations', (req, res, next) => {
+  const payload = {
+    nsw:[
+      'Palm Beach',
+      'Blue Mountain',
+      'Laperuse',
+      'Opera House',
+      'Darling Harbour'
+    ],
+    victoria: [
+      'Royal Botanic Gardens',
+      'National Gallery of Victoria',
+      'Federation Square'
+    ]
+    ,
+    act: [
+      'Australian War Memorial',
+      'New Parliament House',
+      'Lake Burley Griffin',
+      'The National Science and Technology Centre'
+    ]
+  }
+  return res.status(200).send(payload);
+});
+
+app.listen(port, () =>{
+  console.log(`listening to requests on http://localhost:${port}`);
+});
